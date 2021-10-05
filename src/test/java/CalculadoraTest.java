ceab4d736d8e6eeb876a4676b2569abeed3cc537
Cristian Ayala <cdap_13@hotmail.com>
2021-10-05 12:03:58 -0500

    se crea clase calculadora con metodos sumar,restar y multiplicar

---
+++ src/test/java/CalculadoraTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+    Calculadora calculadora = new Calculadora();
+
+    @Test
+    void laSumaDeDosNumerosDebeSerConmutativa() {
+        assertEquals(5, calculadora.sumar(2,3));
+        assertEquals(5, calculadora.sumar(3,2));
+    }
+
+    @Test
+    void restar() {
+        assertEquals(5,calculadora.restar(9,4));
+        assertEquals(-5,calculadora.restar(4,9));
+    }
+
+    @Test
+    void multiplicar() {
+        assertEquals(6,calculadora.multiplicar(2,3));
+    }
+}
